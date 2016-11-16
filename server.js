537442498742c8e50d8c1268cdaf4c665a604553
Vedang <jvedang@gmail.com>
2016-11-16 01:28:51 -0800

    New commit

--- server.js
+++ server.js
@@ -9,9 +9,5 @@ app.get('/', function (req, res) {
 });
 
 //start a server on port 80 and log its start to our console
-var server = app.listen(80, function () {
-
-  var port = server.address().port;
-  console.log('Example app listening on port ', port);
 
 });
