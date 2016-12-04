63442629f9ccd4db99e37e811af59f81d9d37461
Vedang Jadhav <jvedang@gmail.com>
2016-12-03 21:38:00 -0800

    Update server.py

--- server.py
+++ server.py
@@ -7,18 +7,18 @@ mqttc=mqtt.Client()
 mqttc.connect("iot.eclipse.org",1883,60)
 mqttc.loop_start()
 
-print "inside temp"
+print "inside Humidity sensor"
 
 #read temperature
 def read_temp_data():
 	humidity, temperature = Adafruit_DHT.read_retry(11, 4)	
-	print temperature
-	return temperature
+	print humidity
+	return humidity
 
 #publish temperature
 while 1:
 	t=read_temp_data()
-	print "Publishing temperature data"
+	print "Publishing Humidity data"
 	device_uuid=os.environ['RESIN_DEVICE_UUID'];
 	print device_uuid
 	(result,mid)=mqttc.publish("topic/GeneralizedIoT/"+str(device_uuid),t,2)
